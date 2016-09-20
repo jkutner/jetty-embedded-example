a838c83a03c459aea70a3c23e6e1f204ab43d9f2
Joe Kutner <jpkutner@gmail.com>
2016-09-20 10:24:41 -0700

    jetty health check

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,66 +6,63 @@ import org.eclipse.jetty.servlet.*;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.sql.*;
-
-import com.heroku.sdk.jdbc.DatabaseUrl;
+import java.net.*;
+import javax.net.ssl.*;
+import java.security.*;
+import java.security.cert.*;
+import java.io.*;
 
 public class Main extends HttpServlet {
   @Override
   protected void doGet(HttpServletRequest req, HttpServletResponse resp)
       throws ServletException, IOException {
-
-    if (req.getRequestURI().endsWith("/db")) {
-      showDatabase(req,resp);
-    } else {
-      showHome(req,resp);
-    }
-  }
-
-  private void showHome(HttpServletRequest req, HttpServletResponse resp)
-      throws ServletException, IOException {
-    resp.getWriter().print("Hello from Java!");
-  }
-
-  private void showDatabase(HttpServletRequest req, HttpServletResponse resp)
-      throws ServletException, IOException {
-    try {
-      Connection connection = DatabaseUrl.extract().getConnection();
-
-      Statement stmt = connection.createStatement();
-      stmt.executeUpdate("CREATE TABLE IF NOT EXISTS ticks (tick timestamp)");
-      stmt.executeUpdate("INSERT INTO ticks VALUES (now())");
-      ResultSet rs = stmt.executeQuery("SELECT tick FROM ticks");
-
-      String out = "Hello!\n";
-      while (rs.next()) {
-          out += "Read from DB: " + rs.getTimestamp("tick") + "\n";
-      }
-
-      resp.getWriter().print(out);
-    } catch (Exception e) {
-      resp.getWriter().print("There was an error: " + e.getMessage());
-    }
+    resp.getWriter().print("down");
   }
 
   public static void main(String[] args) throws Exception{
-    final Thread mainThread = Thread.currentThread();
-    Runtime.getRuntime().addShutdownHook(new Thread() {
-      public void run() {
-        System.out.println("Goodbye world");
-        try {
-          // mainThread.join();
-        } catch (Exception e) {
-          e.printStackTrace();
-        }
-      }
-    });
-
-    Server server = new Server(Integer.valueOf(System.getenv("PORT")));
-    ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+    final Server server = new Server(Integer.valueOf(System.getenv("PORT")));
+    final ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
     context.setContextPath("/");
     server.setHandler(context);
     context.addServlet(new ServletHolder(new Main()),"/*");
     server.start();
+
+    server.setStopTimeout(10000L);;
+    new Thread() {
+      @Override
+      public void run() {
+        try {
+          while (true) {
+            Thread.sleep(2000);
+
+            URL url = new URL("http://localhost:" + System.getenv("PORT"));
+            HttpURLConnection con = (HttpURLConnection)url.openConnection();
+            con.setRequestMethod("GET");
+            con.setReadTimeout(3000);
+            String health = readInput(con.getInputStream());
+
+            if (!"up".equals(health)) {
+              System.out.println("Stopping Jetty");
+              context.stop();
+              server.stop();
+              break;
+            }
+          }
+        } catch (Exception ex) {
+          System.out.println("Failed to stop Jetty");
+          ex.printStackTrace();
+        }
+      }
+    }.start();
+
     server.join();
+  }
+
+  public static String readInput(InputStream is) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(is));
+    StringBuilder data = new StringBuilder();
+    String s = "";
+    while((s = br.readLine()) != null) data.append(s);
+    return data.toString();
   }
 }
