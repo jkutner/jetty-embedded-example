27d0f6fa17c44b63f65a644232f69daa9c6be8c0
Joe Kutner <jpkutner@gmail.com>
2017-02-03 10:00:38 -0600

    Removed dummy health check code

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,22 +1,18 @@
-import java.io.IOException;
-import javax.servlet.ServletException;
-import javax.servlet.http.*;
 import org.eclipse.jetty.server.Server;
-import org.eclipse.jetty.servlet.*;
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.sql.*;
-import java.net.*;
-import javax.net.ssl.*;
-import java.security.*;
-import java.security.cert.*;
-import java.io.*;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 public class Main extends HttpServlet {
   @Override
   protected void doGet(HttpServletRequest req, HttpServletResponse resp)
       throws ServletException, IOException {
-    resp.getWriter().print("down");
+    resp.getWriter().print("Hello from Jetty");
   }
 
   public static void main(String[] args) throws Exception{
@@ -26,43 +22,6 @@ public class Main extends HttpServlet {
     server.setHandler(context);
     context.addServlet(new ServletHolder(new Main()),"/*");
     server.start();
-
-    server.setStopTimeout(10000L);;
-    new Thread() {
-      @Override
-      public void run() {
-        try {
-          while (true) {
-            Thread.sleep(2000);
-
-            URL url = new URL("http://localhost:" + System.getenv("PORT"));
-            HttpURLConnection con = (HttpURLConnection)url.openConnection();
-            con.setRequestMethod("GET");
-            con.setReadTimeout(3000);
-            String health = readInput(con.getInputStream());
-
-            if (!"up".equals(health)) {
-              System.out.println("Stopping Jetty");
-              context.stop();
-              server.stop();
-              break;
-            }
-          }
-        } catch (Exception ex) {
-          System.out.println("Failed to stop Jetty");
-          ex.printStackTrace();
-        }
-      }
-    }.start();
-
     server.join();
-  }
-
-  public static String readInput(InputStream is) throws IOException {
-    BufferedReader br = new BufferedReader(new InputStreamReader(is));
-    StringBuilder data = new StringBuilder();
-    String s = "";
-    while((s = br.readLine()) != null) data.append(s);
-    return data.toString();
   }
 }
